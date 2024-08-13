c8ef85eac246e20067c6c5c899cb13b751a12964
Ken (V.H) Nguyen <ken.nguyen@ing.com>
2024-08-13 14:08:56 +0200

    Initial commit of an empty Kotlin Multiplatform using Compose to share UI

---
+++ composeApp/src/desktopMain/kotlin/kmp/project/demo/main.kt
@@ -0,0 +1,13 @@
+package kmp.project.demo
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "ComposeDemo",
+    ) {
+        App()
+    }
+}
